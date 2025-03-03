34b5f4fff82a3497ad78046a07212f2299785d13
Nikos Vourdas <32869078+nickvourd@users.noreply.github.com>
2025-03-03 15:32:48 -0800

    Update filetime-converter.py

--- Tools/filetime-converter.py
+++ Tools/filetime-converter.py
@@ -2,7 +2,7 @@
 import datetime
 
 # The given filetime value
-filetime_value = 133850376042623425
+filetime_value = 133850376042623425 # change this
 
 # Convert to datetime
 datetime_value = datetime.datetime.fromtimestamp((filetime_value - 116444736000000000) / 10000000)
