8ef7cfd5fe1c6c95ec05f8fde637c6c99d3c76e2
Nikos Vourdas <32869078+nickvourd@users.noreply.github.com>
2025-03-03 15:30:54 -0800

    Create filetime-converter.py

---
+++ Tools/filetime-converter.py
@@ -0,0 +1,10 @@
+import datetime
+
+# The given filetime value
+filetime_value = 133850376042623425
+
+# Convert to datetime
+datetime_value = datetime.datetime.fromtimestamp((filetime_value - 116444736000000000) / 10000000)
+
+# Print the result in a readable format
+print(datetime_value.strftime('%Y-%m-%d %H:%M:%S'))
