3919d7f8f7ea8b41e3a6e11de2b8c43f39ff1165
Nikos Vourdas <32869078+nickvourd@users.noreply.github.com>
2025-05-12 07:42:44 -0500

    Create IPv6-Deobfuscation.h

---
+++ Tools/IPv6-Deobfuscation.h
@@ -0,0 +1,60 @@
+#pragma once
+
+// #define NumberOfElements <number>
+
+
+typedef NTSTATUS (NTAPI* fnRtlIpv6StringToAddressA)(
+	PCSTR			S, 
+	PCSTR*			Terminator, 
+	PVOID			Addr
+); 
+
+
+BOOL Ipv6Deobfuscation(IN CHAR* Ipv6Array[], IN SIZE_T NmbrOfElements, OUT PBYTE * ppDAddress, OUT SIZE_T * pDSize) {
+
+		PBYTE		pBuffer		= NULL, 
+				TmpBuffer	= NULL; 
+
+		SIZE_T		sBuffSize	= NULL; 
+
+		PCSTR		Terminator	= NULL; 
+
+		NTSTATUS	STATUS		= NULL; 
+
+		// getting RtlIpv6StringToAddressA  address from ntdll.dll
+		fnRtlIpv6StringToAddressA  pRtlIpv6StringToAddressA = (fnRtlIpv6StringToAddressA)GetProcAddress(GetModuleHandle(TEXT("NTDLL")), "RtlIpv6StringToAddressA"); 
+		if (pRtlIpv6StringToAddressA == NULL) {	
+				printf("[!] GetProcAddress Failed With Error : %d \n", GetLastError()); 
+				return FALSE; 
+		}
+		// getting the real size of the shellcode (number of elements * 16 => original shellcode size)
+		sBuffSize = NmbrOfElements * 16; 
+		// allocating mem, that will hold the deobfuscated shellcode
+		pBuffer = (PBYTE)HeapAlloc(GetProcessHeap(), 0, sBuffSize); 
+		if (pBuffer == NULL) {
+			printf("[!] HeapAlloc Failed With Error : %d \n", GetLastError()); 
+			return FALSE; 
+		}
+		// setting TmpBuffer to be equal to pBuffer
+		TmpBuffer = pBuffer; 
+
+
+		// loop through all the addresses saved in Ipv6Array
+		for (int i = 0; i < NmbrOfElements; i++) {
+			// Ipv6Array[i] is a single ipv6 address from the array Ipv6Array
+			if ((STATUS = pRtlIpv6StringToAddressA(Ipv6Array[i], &Terminator, TmpBuffer)) != 0x0) {
+				// if failed ...
+				printf("[!] RtlIpv6StringToAddressA Failed At [%s] With Error 0x%0.8X\n", Ipv6Array[i], STATUS); 
+				return FALSE; 
+			}
+
+			// tmp buffer will be used to point to where to write next (in the newly allocated memory)
+			TmpBuffer = (PBYTE)(TmpBuffer + 16); 
+		}
+
+		*ppDAddress = pBuffer; 
+		*pDSize = sBuffSize; 
+		return TRUE; 
+}
+
+
