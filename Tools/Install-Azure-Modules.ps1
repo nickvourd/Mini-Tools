ffe51bcc8330bd9f7b1502ab9823ea16da3c25c3
Nikos Vourdas <32869078+nickvourd@users.noreply.github.com>
2025-05-14 17:34:02 -0500

    Create Install-Azure-Modules.ps1

---
+++ Tools/Install-Azure-Modules.ps1
@@ -0,0 +1,5 @@
+Install-Module Az -Force -Confirm -AllowClobber -Scope CurrentUser
+Install-Module AzureAD -Force -Confirm -AllowClobber -Scope CurrentUser
+Install-Module Microsoft.Graph -Force -Confirm -AllowClobber -Scope CurrentUser
+Install-Module MSOnline -Force -Confirm -AllowClobber -Scope CurrentUser  
+Install-Module AzureADPreview -Force -Confirm -AllowClobber -Scope CurrentUser
